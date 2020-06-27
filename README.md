96ed3702d18d7c30bd24e4f79220df1f2f26c80f
oekshido <joseugarte2262005@gmail.com>
2020-06-26 22:44:51 -0400

    Finished base program

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Simple markdown-it / AsciiMath editor
 
 ## syntax
 Wrap inline math with `$$`, and multiline math with `$$$`. Read about [AsciiMath here](http://asciimath.org/).
+
+## shortcuts
+Press `Ctrl+S` to download markdown, `Ctrl+H` to download HTML + MathML, and `Ctrl+P` to print or download as PDF. Open markdown files with `Ctrl+O`
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const md = markdownit().use(markdownitMath)
 
 const textarea = document.getElementById("textarea")
 const out = document.getElementById("out")
+const input = document.getElementById("input")
 
 //https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
 function download(file, text) {
@@ -23,5 +24,31 @@ var renderTimeout = null
 textarea.addEventListener("input", ()=>{
 	clearTimeout(renderTimeout)
 	var renderTimeout = setTimeout(()=>{render(); MathJax.typesetPromise()}, 1000)
+})
+
+input.addEventListener("change", ()=>{
+			var fr = new FileReader()
+			fr.addEventListener("load", ()=>{
+				textarea.value = fr.result
+				render()
+				MathJax.typesetPromise()
+			})
+			fr.readAsText(input.files[0])
+})
+
+document.addEventListener("keydown", (e)=>{
+	if(!e.ctrlKey) return
+	else if(e.key == "s"){
+		e.preventDefault()
+		download("file.md", textarea.value)
+	}
+	else if(e.key == "h"){
+		e.preventDefault()
+		download("file.html", md.render(textarea.value))
+	}
+	else if(e.key == "o"){
+		e.preventDefault()
+		input.click()
+	}
 })
 
